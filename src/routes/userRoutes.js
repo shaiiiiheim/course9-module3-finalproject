e2c056f94eb400f680e68523f64a5a6118efebd3
shaiiiiheim <shaiheimallen96@gmail.com>
2025-10-24 18:26:55 -0400

    Adding temporary changes to Github

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,12 @@
+// src/routes/userRoutes.js
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
 
+// Route for user registration
 router.post('/register', registerUser);
+
+// Route for user login
 router.post('/login', loginUser);
+
 module.exports = router;
